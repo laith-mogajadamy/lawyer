8731234b1aa40b14d43e452d20f80db7487b1890
LaithMojjadamy <128228625+laith-mogajadamy@users.noreply.github.com>
2024-11-20 10:19:24 +0300

    Update global.dart

--- lib/core/network/global.dart
+++ lib/core/network/global.dart
@@ -1,5 +1,5 @@
 class Global {
-  static String url = "https://main.briefcaseplatform.com/api/";
+  static String url = "";
   static String local = "http://127.0.0.1:8000/api/";
   static Map<String, String> headers = {
     "Content-type": "application/json",
